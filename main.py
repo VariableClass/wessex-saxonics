ff887d79f9f94440391d62f30684455c7333c628
Callum Veale <callum.veale@gmail.com>
2016-10-07 13:52:50 +0100

    Implemented timezone offset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import jinja2
+import models
 import os
 import webapp2
 
@@ -12,14 +13,20 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         current_time = datetime.datetime.now()
         user = users.get_current_user()
+        userprefs = models.get_userprefs()
         login_url = users.create_login_url(self.request.path)
         logout_url = users.create_logout_url(self.request.path)
+
+        if userprefs:
+            current_time += datetime.timedelta(
+                0, 0, 0, 0, 0, userprefs.tz_offset)
 
         template = template_env.get_template('home.html')
 
         context = {
             'current_time': current_time,
             'user': user,
+            'userprefs': userprefs,
             'login_url': login_url,
             'logout_url': logout_url
         }
---
+++ prefs.py
@@ -0,0 +1,17 @@
+import models
+import webapp2
+
+class PrefsPage(webapp2.RequestHandler):
+    def post(self):
+        userprefs = models.get_userprefs()
+        try:
+            tz_offset = float(self.request.get('tz_offset'))
+            userprefs.tz_offset = tz_offset
+            userprefs.put()
+        except ValueError:
+            # User entered a value that wasn't a float. Ignore for now.
+            pass
+
+        self.redirect('/')
+
+application = webapp2.WSGIApplication([('/prefs', PrefsPage)], debug=True)
